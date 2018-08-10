b2b2d403c86d87cae8db90556e5fb3c2b0bcb159
Basel Issmail <basel.issmail@gmail.com>
2018-08-10 18:23:06 +0300

    use config port variable

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+import config from './server/config/config';
 import { app, server } from './server/server';
 import logger from './server/utils/logger';
 
-app.listen({ port: 3000 }, () => logger.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`));
-
+app.listen({ port: config.port }, () => logger.log(`🚀 Server ready at http://localhost:${config.port}${server.graphqlPath}`));
 
 // var Crawler = require("crawler");
 
