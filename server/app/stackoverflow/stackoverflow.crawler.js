8041529cb0753db4ac13dbd1a6daf0fb8ab0657f
Basel Issmail <basel.issmail@gmail.com>
2018-08-11 23:03:02 +0300

    add pagable search

--- server/app/stackoverflow/stackoverflow.crawler.js
+++ server/app/stackoverflow/stackoverflow.crawler.js
@@ -1,36 +1,51 @@
 import crawler from '../crawler';
 
+const getResolvedResult = (resolve, reject, error, res) => {
+  if (error) {
+    reject(error);
+  } else {
+    const $ = res.$;
+    // resolve({ description: $('span.fav-toggle').html() });
+    const result = [];
+    $('.-item.-job').each(function getDetails() {
+      result.push(
+        {
+          title: $(this).find('h2.job-details__spaced  a').attr('title'),
+          link: $(this).find('h2.job-details__spaced  a').attr('href'),
+          company: $(this).find('div.fc-black-700.fs-body2').clone().find('span')
+            .remove()
+            .end()
+            .text()
+            .replace(/\n/g, '')
+            .replace(/\r/g, '')
+            .trim(),
+        },
+      );
+    });
+    resolve(result);
+  }
+};
+
 const getJobs = async () => new Promise((resolve, reject) => {
   crawler.queue([{
     uri: 'https://stackoverflow.com/jobs',
 
     // The global callback won't be called
     callback(error, res) {
-      if (error) {
-        reject(error);
-      } else {
-        const $ = res.$;
-        // resolve({ description: $('span.fav-toggle').html() });
-        const result = [];
-        $('.-item.-job').each(function getDetails() {
-          result.push(
-            {
-              title: $(this).find('h2.job-details__spaced  a').attr('title'),
-              link: $(this).find('h2.job-details__spaced  a').attr('href'),
-              company: $(this).find('div.fc-black-700.fs-body2').clone().find('span')
-                .remove()
-                .end()
-                .text()
-                .replace(/\n/g, '')
-                .replace(/\r/g, '')
-                .trim(),
-            },
-          );
-        });
-        resolve(result);
-      }
+      getResolvedResult(resolve, reject, error, res);
     },
   }]);
 });
 
-export { getJobs };
+const searchJobs = async input => new Promise((resolve, reject) => {
+  crawler.queue([{
+    uri: `https://stackoverflow.com/jobs?sort=i&q=${input.keyWord}&pg=${input.pageNumber}`,
+
+    // The global callback won't be called
+    callback(error, res) {
+      getResolvedResult(resolve, reject, error, res);
+    },
+  }]);
+});
+
+export { getJobs, searchJobs };
--- server/app/stackoverflow/stackoverflow.graphql.js
+++ server/app/stackoverflow/stackoverflow.graphql.js
@@ -1,6 +1,6 @@
 import { gql } from 'apollo-server-express';
 import Job from '../job.graphql';
-import { getJobs } from './stackoverflow.crawler';
+import { getJobs, searchJobs } from './stackoverflow.crawler';
 
 // Construct a schema, using GraphQL schema language
 const stackoverflowDefs = gql`
@@ -8,12 +8,23 @@ const stackoverflowDefs = gql`
     allStackoverflowJobs: [Job]
   }
 
+  input SearchInput {
+    keyWord: String!
+    pageNumber: Int
+  } 
+
+  type Mutation {
+    searchStackoverflowJobs(input: SearchInput): [Job]
+  }
 `;
 
 // Provide resolver functions for your schema fields
 const stackoverflowResolvers = {
   Query: {
     allStackoverflowJobs: () => getJobs(),
+  },
+  Mutation: {
+    searchStackoverflowJobs: (root, { input }) => searchJobs(input),
   },
 };
 
